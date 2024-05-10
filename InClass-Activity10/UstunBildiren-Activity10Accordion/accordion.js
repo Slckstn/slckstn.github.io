299889401d58689fa180084c619ad78f93e2e7c9
Selçuk Üstün <93039061+Slckstn@users.noreply.github.com>
2024-05-10 19:30:38 +0300

    V2

--- InClass-Activity10/UstunBildiren-Activity10Accordion/accordion.js
+++ InClass-Activity10/UstunBildiren-Activity10Accordion/accordion.js
@@ -1,16 +1,11 @@
 $(function() {
     var emailPattern = /\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}\b/;
 
-    // move the focus to the first text box
     $("#arrival_date").focus();
 
-    // the handler for the submit event of the form
-    // executed when the submit button is clicked
     $("#reservation_form").submit(
         function(event) {
             var isValid = true;
-
-            // validate the requested arrival date
             var arrivalDate = $("#arrival_date").val().trim();
             if (arrivalDate == "") {
                 $("#arrival_date").next().text("This field is required.");
@@ -20,7 +15,6 @@ $(function() {
             }
             $("#arrival_date").val(arrivalDate);
 
-            // validate the number of nights
             var nights = $("#nights").val().trim();
             if (nights == "") {
                 $("#nights").next().text("This field is required.");
@@ -33,7 +27,6 @@ $(function() {
             }
             $("#nights").val(nights);		
 
-            // validate the name entry
             var name = $("#name").val().trim();
             if (name == "") {
                 $("#name").next().text("This field is required.");
@@ -45,7 +38,6 @@ $(function() {
             }
             $("#name").val(name);
 
-            // validate the email entry with a regular expression
             var email = $("#email").val();
             if (email == "") { 
                 $("#email").next().text("This field is required.");
@@ -58,7 +50,6 @@ $(function() {
             }
             $("#email").val(email); 
 
-            // validate the phone number
             var phone = $("#phone").val().trim();
             if (phone == "") { 
                 $("#phone").next().text("This field is required.");
@@ -68,12 +59,11 @@ $(function() {
             }
             $("#phone").val(phone);
 
-            // prevent the submission of the form if any entries are invalid 
             if (isValid == false) {
                 event.preventDefault();				
             }
-        } // end function
-    );	// end submit
+        } 
+    );	
 
     $("#tabs").tabs();
     $("#arrival_date").datepicker({
